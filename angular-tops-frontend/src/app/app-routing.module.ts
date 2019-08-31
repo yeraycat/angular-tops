43ac7692dd41b4d408b43216bb14069ace203350
Yeray Catalá <yeray.cv@protonmail.com>
2019-08-31 04:12:28 +0200

    Added rest of main routes

--- angular-tops-frontend/src/app/app-routing.module.ts
+++ angular-tops-frontend/src/app/app-routing.module.ts
@@ -3,8 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'top',
+    loadChildren: './pages/top-list/top-list.module#TopListModule'
+  },
+  {
+    path: 'add-listing',
+    loadChildren: './pages/add-listing/add-listing.module#AddListingModule'
+  },
+  {
     path: 'categories',
     loadChildren: './pages/categories/categories.module#CategoriesModule'
+  },
+  {
+    path: 'vote',
+    loadChildren: './pages/vote-landing/vote-landing.module#VoteLandingModule'
   }
 ];
 
