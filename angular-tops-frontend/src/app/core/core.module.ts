bf380bcb37599ee69e9c825f6ab5a565a82ccbe2
Yeray Catalá <yeray.cv@protonmail.com>
2019-08-31 20:14:50 +0200

    Installed and configured Store Devtools

--- angular-tops-frontend/src/app/core/core.module.ts
+++ angular-tops-frontend/src/app/core/core.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemApiService } from './services/in-mem-api.service';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemApiService),
-    StoreModule.forRoot({})
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      name: 'Ng Tops Devtools',
+      maxAge: 25,
+      logOnly: environment.production
+    }),
   ]
 })
 export class CoreModule { }
